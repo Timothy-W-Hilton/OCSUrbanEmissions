41d7e14fa01cd1a2080e86ade80eedf6fbf43c1c
twhilton@ucsc.edu <thilton@himec01.pic.es>
2018-01-30 14:32:05 +0100

    update make_bcn_griddesc.sh to include SiB 1.0 x 1.25 grid in GRIDDESC

--- make_bcn_griddesc.sh
+++ make_bcn_griddesc.sh
@@ -8,7 +8,7 @@ export GRIDDESC_BCN=$PWD/GRIDDESC_BCN
 # get everything wrfgriddesc needed.
 # as of 30 Jan 2018 the PIC installation of Models-3 I/O API requires
 # netCDF version 3 input.  Therefore use "-3" flag to ncks.
-ncks -3 -o -v Times,XLAT,XLONG,T,P,U,V,W,ZS /software/co2flux/ilavrador/WRFOUT_STEM/wrfout_d01_2016-02-01_00:00:00 $TMP_WRFOUT
+ncks -3 -v Times,XLAT,XLONG,T,P,U,V,W,ZS /software/co2flux/ilavrador/WRFOUT_STEM/wrfout_d01_2016-02-01_00:00:00 $TMP_WRFOUT
 
 # WRFFILE <path name for input WRF-netcdf file>
 export WRFFILE=$TMP_WRFOUT
@@ -19,7 +19,22 @@ export DOTGRID=DOT_BCND01  # <GRIDDESC name for       dot-point grid>
 export STXGRID=STX_BCND01  # <GRIDDESC name for X-stagger-point grid>
 export STYGRID=STY_BCND01  # <GRIDDESC name for Y-stagger-point grid>
 
-# now run wrfgriddesc
-wrfgriddesc
+# remove GRIDDESC if it already exists
+if [ -e "$OUTDESC" ]
+then
+   rm -fv $OUTDESC
+fi
+# now run wrfgriddesc, answering "Y" at the "continue" prompt
+wrfgriddesc <<EOF
+Y
+EOF
 
 rm -fv $TMP_WRFOUT
+
+# now add description of the native SiB grid into the GRIDDESC file
+awk -v n=2 -v s=" 'LAT_LON'\n     1\t0.0D0\t0.0D0\t0.0D0\t0.0D0\t0.0D0" 'NR == n {print s} {print}' GRIDDESC_BCN > tmp_out
+awk -v n=17 -v s=" 'SIB_NATIVE'\n 'LATLON' -180, -90, 1.25, 1., 288, 180, 1" 'NR == n {print s} {print}' tmp_out > tmp_out2
+
+# clean up
+rm -f tmp_out
+mv tmp_out2 GRIDDESC_BCN
