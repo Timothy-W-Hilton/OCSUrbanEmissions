a5c99f1b5a7d756412a916fdb2f0e0358550ca48
twhilton@ucsc.edu <thilton@himec01.pic.es>
2018-02-01 19:18:14 +0100

    update makefile to use netCDF library v. 4.1.3 (IMPORTANT)
    - Isabel's Barcelona WRFOUT files are netCDF version 4.  The previous
      makefile used netCDF v. 3.6.3 and could not read the WRFOUT files.

--- Preprocessor/preprocess.mk
+++ Preprocessor/preprocess.mk
@@ -8,8 +8,8 @@ LD=gfortran
 
 FFLAGS   = -c -O $(APINCL) $(STEMINCL) -g -fopenmp -Wtabs
 LDFLAGS  = -O -g77libs -g -fopenmp -Wtabs
-APINCL=-I/usr/include -I/software/co2flux/LIBRARIES/netcdf-3.6.3/include -I/nfs/pic.es/user/t/thilton/Software/ioapi-3.2_tim/Linux2_x86_64gfort
-APILIB=-L/usr/lib64 -L/nfs/pic.es/user/t/thilton/Software/ioapi-3.2_tim/Linux2_x86_64gfort -L$(PWD) -lioapi -L/software/co2flux/LIBRARIES/netcdf-3.6.3/lib -lnetcdf
+APINCL=-I/usr/include -I/software/co2flux/LIBRARIES/netcdf-4.1.3/include -I/nfs/pic.es/user/t/thilton/Software/ioapi-3.2_tim/Linux2_x86_64gfort
+APILIB=-L/usr/lib64 -L/nfs/pic.es/user/t/thilton/Software/ioapi-3.2_tim/Linux2_x86_64gfort -L$(PWD) -lioapi -L/software/co2flux/LIBRARIES/netcdf-4.1.3/lib -lnetcdff
 
 CMD=$(PROGRAM).x
 TOPOCMD=$(TOPO).x
