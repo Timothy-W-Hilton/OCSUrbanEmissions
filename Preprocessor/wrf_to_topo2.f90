082c6bada2df9cbe6b053b4b93c98f618e12912a
twhilton@ucsc.edu <thilton@himec01.pic.es>
2018-02-01 19:27:14 +0100

    read GRIDDESC file in wrf_to_topo.f90
    - I want to make it take the grid information from griddesc to avoid
      needing to hard-code grid parameters.  The GRIDDESC file cab be
      written directly from a wrfout file using
      wrfgriddesc (https://www.cmascenter.org/ioapi/documentation/all_versions/html/WRFGRIDDESC.html)

--- Preprocessor/wrf_to_topo2.f90
+++ Preprocessor/wrf_to_topo2.f90
@@ -1,10 +1,8 @@
 !--------------------------------------------------
 !REQUIREMENTS
-!EDSS/Models-3 I/O API logical names WRF_FILE and OUTPUT must be set.
-!WRF_FILE should point to the WRF output file to be converted to a
-!TOPO file.  OUTPUT should point to the TOPO file to create.
-!
-!
+!EDSS/Models-3 I/O API logical names WRF_FILE, OUTPUT, and GRIDDESC must
+!be set.  WRF_FILE should point to the WRF output file to be converted
+!to a TOPO file.  OUTPUT should point to the TOPO file to create.
 !--------------------------------------------------
 program wrf_to_ioapi
       USE M3UTILIO
@@ -13,8 +11,9 @@ program wrf_to_ioapi
 
 
    ! variables list
-   CHARACTER(len=255) :: infile
-   CHARACTER(len=255) :: outfile 
+      CHARACTER(len=255) :: infile
+      CHARACTER(len=255) :: outfile
+      CHARACTER(len=255) :: griddesc
    integer::LOGDEV
    integer::jdate,jtime !integers used for TFLAG during write
    integer::ncid, xlatid, xlonid, hgtid, MFid, LUid !netcdf variable id's
@@ -22,23 +21,25 @@ program wrf_to_ioapi
    integer::x,y
    real, DIMENSION(:,:), ALLOCATABLE :: xlat
    real, DIMENSION(:,:), ALLOCATABLE :: xlon
-   real, DIMENSION(:,:), ALLOCATABLE :: hgt 
-   real, DIMENSION(:,:), ALLOCATABLE :: MF 
-   INTEGER, DIMENSION(:,:), ALLOCATABLE :: LU 
+   real, DIMENSION(:,:), ALLOCATABLE :: hgt
+   real, DIMENSION(:,:), ALLOCATABLE :: MF
+   INTEGER, DIMENSION(:,:), ALLOCATABLE :: LU
 
-   print*, '---------------checking invironemnt:   '
+   print*, '---------------checking environment:   '
    CALL get_environment_variable("WRF_FILE", infile)
-   CALL get_environment_variable("OUTPUT",outfile)
-   WRITE (*,*) TRIM(infile)
-   WRITE (*,*) TRIM(outfile)
+      CALL get_environment_variable("OUTPUT",outfile)
+   CALL get_environment_variable("GRIDDESC",griddesc)
+      WRITE (*,*) TRIM(infile)
+      WRITE (*,*) TRIM(outfile)
+      WRITE (*,*) TRIM(griddesc)
    print*,"infile###############",infile
 
 
    ! allocate the input data variable
    !nrows = 310
    !ncols = 202
-   nrows = 50 
-   ncols = 50 
+   nrows = 50
+   ncols = 50
 !   zlevs = 59
    allocate(xlat(ncols,nrows))
    allocate(xlon(ncols,nrows))
@@ -50,37 +51,37 @@ program wrf_to_ioapi
    ! Open the net cdf file. NF90_NOWRITE Just means read only.
    print*,"Opening file..."
    call check( nf90_open(infile, NF90_NOWRITE, ncid))
-   
+
    ! get the varid of the data
    print*,"Reading data variable XLAT"
    call check( nf90_inq_varid(ncid, "XLAT", xlatid))
-   call check( nf90_get_var(ncid,xlatid,xlat)) 
-   
+   call check( nf90_get_var(ncid,xlatid,xlat))
+
    print*,"Reading data variable XLONG"
    call check( nf90_inq_varid(ncid, "XLONG", xlonid))
-   call check( nf90_get_var(ncid,xlonid,xlon)) 
-   
+   call check( nf90_get_var(ncid,xlonid,xlon))
+
    print*,"Reading data variable HGT"
    call check( nf90_inq_varid(ncid, "HGT", hgtid))
-   call check( nf90_get_var(ncid,hgtid,hgt)) 
-   
+   call check( nf90_get_var(ncid,hgtid,hgt))
+
    print*,"Reading data variable MAPFAC_M"
    call check( nf90_inq_varid(ncid, "MAPFAC_M", MFid))
-   call check( nf90_get_var(ncid,MFid,MF)) 
+   call check( nf90_get_var(ncid,MFid,MF))
 
    print*,"Reading data variable LU_INDEX"
    call check( nf90_inq_varid(ncid, "LU_INDEX", LUid))
-   call check( nf90_get_var(ncid,LUid,LU)) 
+   call check( nf90_get_var(ncid,LUid,LU))
    ! read the data
 
    print*, "Data imported succsessfully!"
-   
+
    ! Check the data.
 !   print*, "Checking the data:"
 !   do x = 1, ncols
 !      do y = 1, nrows
 !            print *, "lat:", xlat(x,y), ", lon:", xlon(x,y),", hgt:", hgt(y, x)
-!      
+!
 !      end do
 !   end do
 
@@ -128,23 +129,23 @@ program wrf_to_ioapi
       vname3d(3) = 'TOPO'
       vdesc3d(3) = 'Topograph'
       vtype3d(3)=m3real
-      
+
       vglvs3d(4)=1.             ! levels in meter
       units3d(4)='RATIO'
       vname3d(4) = 'MFACTOR'
       vdesc3d(4) = 'MASS MAP FACTOR'
       vtype3d(4)=m3real
-      
+
       vglvs3d(5) = 1.             ! levels in meter
       units3d(5) = 'Integer CATEGORY'
       vname3d(5) = 'LANDUSE'
       vdesc3d(5) = 'LANDUSE CATEGORIES'
       vtype3d(5)=m3int
       !sdate3d = 2010185
-      !stime3d =  120000  
+      !stime3d =  120000
       !tstep3d = 003000
       !sdate3d = 1999365
-      sdate3d = 2015064 
+      sdate3d = 2015064
       stime3d = 0
       tstep3d = 0
 
@@ -159,34 +160,34 @@ program wrf_to_ioapi
       else
          print*,'Reading from previously created file!'
          if (.not. DESC3('OUTPUT') ) then ! if exit, get information
-            print*, 'Error getting info from OUTPUT nc' 
+            print*, 'Error getting info from OUTPUT nc'
             stop
          endif
       endif
 
-   
+
       ! Setup time and iteration information.
-      jdate = 0 
+      jdate = 0
       jtime = 0
-      
-      
+
+
       ! Attempt an I/O api data write. For whatever reason the convention is to
       !     loop through time and write one 2d grid at a time.
       if(.not.write3('OUTPUT',vname3d(1),jdate,jtime,xlat)) then
          print*,'writing error'
          stop
-      endif 
+      endif
 
       if(.not.write3('OUTPUT',vname3d(2),jdate,jtime,xlon)) then
          print*,'writing error'
          stop
-      endif 
+      endif
 
       if(.not.write3('OUTPUT',vname3d(3),jdate,jtime,hgt)) then
          print*,'writing error'
          stop
-      endif 
-      
+      endif
+
       if(.not.write3('OUTPUT',vname3d(4),jdate,jtime,MF)) then
          print*,'writing error'
          stop
@@ -199,12 +200,12 @@ program wrf_to_ioapi
       ! Close I/O api
       print*, 'SHUT3()=',SHUT3()
 
- 
+
    contains
       subroutine check(status)
          integer, intent ( in) :: status
-    
-         if(status /= nf90_noerr) then 
+
+         if(status /= nf90_noerr) then
             print *, trim(nf90_strerror(status))
          stop "Stopped"
        end if
