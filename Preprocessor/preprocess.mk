211e609a0b459175d78c6d161b41a559af3ae0d4
twhilton@ucsc.edu <thilton@himec01.pic.es>
2018-02-02 22:59:14 +0100

    use /software I/O API installation

--- Preprocessor/preprocess.mk
+++ Preprocessor/preprocess.mk
@@ -7,9 +7,9 @@ FC=gfortran
 LD=gfortran
 
 FFLAGS   = -c -O $(APINCL) $(STEMINCL) -g -fopenmp -Wtabs
-LDFLAGS  = -O -g77libs -g -fopenmp -Wtabs
-APINCL=-I/usr/include -I/software/co2flux/LIBRARIES/netcdf-4.1.3/include -I/nfs/pic.es/user/t/thilton/Software/ioapi-3.2_tim/Linux2_x86_64gfort
-APILIB=-L/usr/lib64 -L/nfs/pic.es/user/t/thilton/Software/ioapi-3.2_tim/Linux2_x86_64gfort -L$(PWD) -lioapi -L/software/co2flux/LIBRARIES/netcdf-4.1.3/lib -lnetcdff
+LDFLAGS  = -O0 -g77libs -g -fopenmp -Wtabs
+APINCL=-I/usr/include -I/software/co2flux/LIBRARIES/netcdf-4.1.3/include -I/software/co2flux/LIBRARIES/ioapi-3.2/Linux2_x86_64gfort/
+APILIB=-L/usr/lib64 -L/software/co2flux/LIBRARIES/ioapi-3.2/Linux2_x86_64gfort/ -lioapi -L/software/co2flux/LIBRARIES/netcdf-4.1.3/lib -lnetcdff
 
 CMD=$(PROGRAM).x
 TOPOCMD=$(TOPO).x
